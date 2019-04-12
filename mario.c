90084ef16dd7f49622f5afdae393da0424a2dbd2
LoonyGryphon <LoonyGryphon@users.noreply.github.com>
2019-04-12 06:24:32 +0000

    20190412T062329Z

---
+++ mario.c
@@ -0,0 +1,50 @@
+//mario.c
+//Harrison Oates 12/04/19
+//This is CS50
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void);
+
+int get_positive_int(string Height);
+
+//Asks for imput
+int main (void)
+{
+printf("Welcome to Mario Castle Maker!\n");
+printf("Please enter in a number between 1 and 8\n");
+int i = get_positive_int("Height: ");
+    
+    
+//loop that makes pyramid.
+//i is height
+//j is space
+//k is number of rows
+//l is hashes
+for (int k = 0; k < i; k++)
+{
+  for (int j = (i-k); j > 0; j--)
+    {
+    printf(" ");
+    }
+  for (int l = 1; l <= (k+1); l++)
+  {
+      printf("#");
+  }
+    printf("\n");
+}
+    return 0;
+}
+
+//prompt for height
+int get_positive_int(string Height)
+{
+    int n;
+    do
+    {
+        n = get_int("%s", Height);
+    }
+    while ((n < 0) || (n > 8));
+    return n;
+}
