ed6f8f1fdbb91d97ff1f4850e502e0c69b4e01e3
LoonyGryphon <48871176+LoonyGryphon@users.noreply.github.com>
2019-06-23 09:05:33 +1000

    Add files via upload

---
+++ Bob Hello.py
@@ -0,0 +1,12 @@
+#hello Bob
+
+name = input('Name: \n')
+if (name == 'Bob')
+    print('Hello, ', name)
+
+else if (name == 'Alice')
+    print('Hello, ', name)
+else
+    print('Go Away!\n')
+
+return 0
---
+++ steps to 1.py
@@ -0,0 +1,18 @@
+
+n = int(input('Any number: '))
+
+counter = 0
+while n!= 1:
+    if int(n) % 2 == 0:
+        n=n/2
+        print(n)
+        counter += 1
+    else:
+        n = 3*n+1
+        print(n)
+        counter += 1
+
+if n == 1:
+    print('Number of steps: ', counter)
+
+
